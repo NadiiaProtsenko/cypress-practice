45ee56f1ab9cbb0be5f9522c0e2369a4eeb3871a
Nadiia <philimonovanadiya@gmail.com>
2024-04-07 00:27:26 +0300

    Add tests to verify incorrect password and username

--- cypress/e2e/form-tests/auth-form.cy.js
+++ cypress/e2e/form-tests/auth-form.cy.js
@@ -1,15 +1,18 @@
-
-describe('Login Test', () => {
-    it('Should display error message for invalid password', () => {
+describe('Login page', () => {
+    it('should display error message for invalid password', () => {
       cy.visit('https://the-internet.herokuapp.com/login');
-      
-      cy.get('#username').type('tomsmith');
-      
-      cy.get('#password').type('invalidpassword');
-      
-      cy.get('button[type="submit"]').click();
-      
-      cy.get('#flash').should('contain', 'Your password is invalid!');
+      cy.get('#username').type('existing_username');
+      cy.get('#password').type('invalid_password');
+      cy.contains('Login').click();
+      cy.contains('Your password is invalid!').should('be.visible');
+    });
+  
+    it('should display error message for invalid username', () => {
+      cy.visit('https://the-internet.herokuapp.com/login');
+      cy.get('#username').type('invalid_username');
+      cy.get('#password').type('any_password');
+      cy.contains('Login').click();
+      cy.contains('Your username is invalid!').should('be.visible');
     });
   });
   
